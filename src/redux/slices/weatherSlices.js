238f896bd7bc841b8531e182e16e7ecdff0a745a
Brajesh-chandra <nikt7429@gmail.com>
2022-10-20 19:48:29 +0530

    first commit

--- src/redux/slices/weatherSlices.js
+++ src/redux/slices/weatherSlices.js
@@ -6,7 +6,7 @@ export const fetchWeatherAction = createAsyncThunk(
   async (payload, { rejectWithValue, getState, dispatch }) => {
     try {
       const { data } = await axios.get(
-        `http://api.openweathermap.org/data/2.5/weather?q=${payload}&appid=094b545dde613af5667ba10639a224f8&units=imperial`
+        `http://api.openweathermap.org/data/2.5/weather?q=${payload}&appid=af645f1b3bd9f3236f3a48880bccad58&units=imperial`
       );
 
       return data;
